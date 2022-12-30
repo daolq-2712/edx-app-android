380046cc8c916eddf62f20d0a1df0271ce9e5c40
le.quang.dao <le.quang.dao@sun-asterisk.com>
2022-12-30 16:35:18 +0700

    Change package

--- OpenEdXMobile/src/main/java/org/edx/mobile/view/RegisterActivity.java
+++ OpenEdXMobile/src/main/java/org/edx/mobile/view/RegisterActivity.java
@@ -187,7 +187,7 @@ public class RegisterActivity extends BaseFragmentActivity
             }
         });
 
-        getRegistrationForm();
+//        getRegistrationForm();
 
         hideSoftKeypad();
         tryToSetUIInteraction(true);
